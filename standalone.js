6fcba331ccb23b76fd57ef008a2b65cda01431e6
Rik Hoffbauer <Rikhoffbauer@gmail.com>
2015-12-16 22:10:40 +0100

    fixed exporting a variable with the wrong name

--- standalone.js
+++ standalone.js
@@ -3,18 +3,14 @@
  */
 import index from './index';
 
-const globalName = 'View';
+const globalName = 'Translator';
 
 if (typeof window.define == 'function' && window.define.amd) {
   window.define(globalName, function () {
     return index;
   });
-  window.define('riot', function () {
-    return index.riot;
-  });
 } else {
   window[globalName] = index;
-  window.riot = index.riot;
 }
 
 export default index;
